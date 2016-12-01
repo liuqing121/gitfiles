32c8616285ed8208e95128147e9ac76e786de4a6
liuqing <469005693@qq.com>
2016-12-01 18:28:58 +0800

    List

---
+++ STL/STL/test_list.h
@@ -0,0 +1,36 @@
+//#include<iostream>
+//#include<list>
+//using namespace std;
+//void TestList()
+//{
+//	list<int> li;
+//	li.push_front(1);
+//	li.push_front(2);
+//	li.push_front(3);
+//	li.push_front(4);
+//	li.push_front(5);
+//	list<int>::iterator it;
+//	for (it = li.begin(); it != li.end(); it++)
+//	{
+//		cout << *it << " ";
+//	}
+//	cout << endl;
+//	li.insert(li.begin(), 10);
+//	for (it = li.begin(); it != li.end(); it++)
+//	{
+//		cout << *it << " ";
+//	}
+//	cout << endl;
+//	li.insert(li.end(), 3, 6);
+//	cout << endl;
+//	for (it = li.begin(); it != li.end(); it++)
+//	{
+//		cout << *it << " ";
+//	}
+//	cout << endl;
+//}
+
+// list::sort
+
+
+
